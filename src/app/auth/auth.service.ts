6c560a5c01166b7097afd861365e091dd7a0e93b
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-23 00:30:25 +0530

    Added Sign  up in auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 interface UsernameReponse {
   available: boolean;
+}
+interface SignUpCredentials {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+interface SignUpResponse {
+  username: string;
 }
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) {}
+
   usernameAvailable(username: string) {
-    return this.http.post<UsernameReponse>(
-      'https://api.angular-email.com/auth/username',
-      {
-        username,
-      }
-    );
+    return this.http.post<UsernameReponse>(`${this.rootUrl}/auth/username`, {
+      username,
+    });
+  }
+
+  signup(credentials: SignUpCredentials) {
+    return this.http
+      .post<SignUpResponse>(`${this.rootUrl}/auth/signuup`, credentials)
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
   }
 }
