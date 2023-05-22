ada36717218c16d2bcacfedc08018b529aaee2fd
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-22 22:34:58 +0530

    Added reusable input

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     AuthRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    SharedModule,
   ],
 })
 export class AuthModule {}
